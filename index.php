a1fd205a31dec05d1337053acdd73e8960a0abdd
Ravli Fathian <ravlifathian2005@gmail.com>
2025-01-01 20:52:17 +0700

    Main Code

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('head.php') ?>
+</head>
+
+<body>
+    <?php include('navbar.php') ?>
+    <?php include('header.php') ?>
+    <?php include('content.php') ?>
+    <?php include('footer.php') ?>
+</body>
+
+</html>
